dbc3b729b288e695f9c0f3cbd16e76a9629d1cef
Sergei Zheleznov <sksdes@gmail.com>
2020-05-09 18:24:09 +0200

    testing threejs

--- sp-threejs/src/webparts/colladaViewer/components/ColladaViewer.tsx
+++ sp-threejs/src/webparts/colladaViewer/components/ColladaViewer.tsx
@@ -2,23 +2,56 @@ import * as React from 'react';
 import styles from './ColladaViewer.module.scss';
 import { IColladaViewerProps } from './IColladaViewerProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import * as THREE from 'three';
+import {ColladaLoader} from 'three/examples/jsm/loaders/ColladaLoader';
+
+const model: any = require('./../assets/elf.dae');
 
 export default class ColladaViewer extends React.Component<IColladaViewerProps, {}> {
+
+  private renderer: THREE.WebGLRenderer;
+
+  public componentDidMount() {
+
+
+    const container = document.getElementById( 'viewport' );
+    let elf = null;
+
+
+    const camera = new THREE.PerspectiveCamera( 120, window.innerWidth / window.innerHeight, 0.1, 2000 );
+    camera.position.set( 8, 10, 8 );
+    camera.lookAt( 0, 3, 0 );
+
+    const scene = new THREE.Scene();
+    const loadingManager = new THREE.LoadingManager( () => {
+      scene.add( elf );
+    } );
+
+    const loader = new ColladaLoader( loadingManager );
+    loader.load( model, ( collada ) => {
+       elf = collada.scene;
+    } );
+
+    const ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
+    scene.add( ambientLight );
+
+    const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.8 );
+    directionalLight.position.set( 1, 1, 0 ).normalize();
+    scene.add( directionalLight );
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setPixelRatio( window.devicePixelRatio );
+    this.renderer.setSize( container.offsetWidth, container.offsetHeight);
+
+    container.appendChild( this.renderer.domElement );
+    this.renderer.render( scene, camera );
+  }
+
   public render(): React.ReactElement<IColladaViewerProps> {
+
     return (
-      <div className={ styles.colladaViewer }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
+      <div id="viewport" className={ styles.colladaViewer }>
+
       </div>
     );
   }
