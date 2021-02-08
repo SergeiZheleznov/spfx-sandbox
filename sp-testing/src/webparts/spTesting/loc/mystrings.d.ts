d757e97f47fbb368a0176fef8b111c52c724da23
Sergei Zheleznov <sksdes@gmail.com>
2021-02-08 18:31:44 +0100

    Add example sp-testing

---
+++ sp-testing/src/webparts/spTesting/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpTestingWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpTestingWebPartStrings' {
+  const strings: ISpTestingWebPartStrings;
+  export = strings;
+}
